53826ce734ad463705d336ab338af9920c5be114
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-09-25 21:49:43 +0100

    Updated DTOs for create user on and user app

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,19 @@
-import { IsString, IsEmail, MinLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsEmail, MinLength, IsNotEmpty, IsStrongPassword } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
+  @IsNotEmpty()
   @IsString()
   username: string;
 
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
+  @IsStrongPassword()
   @MinLength(6)
   password: string;
 }
