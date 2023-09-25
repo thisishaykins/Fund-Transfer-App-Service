7350e22161a297b0b2a614963d74acd8ba754f07
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-09-25 21:46:03 +0100

    Added swagger configs for documentations...

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,22 @@ import { config } from 'dotenv';
 config(); // Load environment variables from .env file
 
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // const options = new DocumentBuilder()
-  //   // .addBearerAuth()
-  //   .setTitle('Fund Transfer Service App')
-  //   .setDescription('Fund Transfer Service API Documentation')
-  //   .setVersion('0.0.1')
-  //   .addTag('Fintech')
-  //   .build();
+  const options = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Fund Transfer Service App')
+    .setDescription('Fund Transfer Service API Documentation')
+    .setVersion('0.0.1')
+    .addTag('Fintech')
+    .build();
 
-  // const document = SwaggerModule.createDocument(app, options);
-  // SwaggerModule.setup('docs', app, document);
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.APP_PORT || 3000);
 }
