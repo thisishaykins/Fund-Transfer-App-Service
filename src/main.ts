c4e4d1c07fe8494f633000eee9b7e7ebcda5dc96
Akinshola Samuel <akinsholasamuel@gmail.com>
2023-09-25 21:08:51 +0100

    Added dotenv to load .env variable on the app...

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
+import { config } from 'dotenv';
+config(); // Load environment variables from .env file
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // const options = new DocumentBuilder()
+  //   // .addBearerAuth()
+  //   .setTitle('Fund Transfer Service App')
+  //   .setDescription('Fund Transfer Service API Documentation')
+  //   .setVersion('0.0.1')
+  //   .addTag('Fintech')
+  //   .build();
+
+  // const document = SwaggerModule.createDocument(app, options);
+  // SwaggerModule.setup('docs', app, document);
+
+  await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
